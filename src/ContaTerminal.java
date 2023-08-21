76b951b4340f4d0977ea147e06a56c1aaee60404
Fa-biano <fbiano.souza@gmail.com>
2023-08-21 19:05:20 -0300

    feat: implements ContaTerminal class

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.text.MessageFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double balance = 237.48;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Por favor, digite as informações solicitadas abaixo!");
+            System.out.println("O número da sua agência:");
+            int agencynumber = scanner.nextInt();
+            
+            System.out.println("O número da Conta (informe o dígito, exemplo xxx-x):");
+            String accountnumber = scanner.next();
+            
+            System.out.println("Confirme seu nome:");
+            String fullName = scanner.next();
+
+            
+            String response = MessageFormat.format(
+                "Olá {0}, obrigado por criar uma conta em nosso banco, sua agência é {1}, conta {2} e seu saldo {3} já está disponível para saque.",
+                fullName,
+                agencynumber,
+                accountnumber,
+                balance
+            );
+
+            System.out.println(response);
+        }
     }
 }
